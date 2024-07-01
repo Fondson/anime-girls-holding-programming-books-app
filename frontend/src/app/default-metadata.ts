699027a2b128ab3479e6444839df99ea4a5c2779
Fondson Tran <fondsont@gmail.com>
2024-07-01 02:55:18 -0400

    Add metadataBase to defaultMetadata

--- frontend/src/app/default-metadata.ts
+++ frontend/src/app/default-metadata.ts
@@ -2,6 +2,7 @@ import { cloneDeep } from 'lodash-es'
 import { Metadata } from 'next'
 
 const defaultMetadataObj: Metadata = {
+  metadataBase: new URL(process.env.BASE_URL || ''),
   title: {
     template: '%s | Anime Girls Holding Programming Books',
     default: 'Anime Girls Holding Programming Books',
