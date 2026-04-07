8b45a67255caba0469f1b3a2e4682abfabc25f10
Fondson Tran <fondsont@gmail.com>
2026-04-07 01:08:11 -0700

    Add redirects for the application and set canonical metadata URL for SEO optimization

--- frontend/src/app/default-metadata.ts
+++ frontend/src/app/default-metadata.ts
@@ -3,6 +3,9 @@ import { Metadata } from 'next'
 
 const defaultMetadataObj: Metadata = {
   metadataBase: new URL(process.env.BASE_URL || ''),
+  alternates: {
+    canonical: 'https://anime-girls-holding-programming-books.com/',
+  },
   title: {
     template: '%s | Anime Girls Holding Programming Books',
     default: 'Anime Girls Holding Programming Books',
